16cd413f397a7c2d1f377c3017baa06515eb9f89
Mukund Sutar <mukundsutar1512@gmail.com>
2023-03-19 23:34:04 +0530

    Test No.2 - Final successfully copied button to clipboard

--- js/copy.js
+++ js/copy.js
@@ -1,5 +1,11 @@
-function copyText(){
-  const text = document.getElementById('text').innerText
+function copyTextEmail(){
+  const text = document.getElementById('text-email').innerText
+  const btnText = document.getElementById('btn')
+  navigator.clipboard.writeText(text);
+}
+
+function copyTextPhone(){
+  const text = document.getElementById('text-phone').innerText
   const btnText = document.getElementById('btn')
   navigator.clipboard.writeText(text);
 }
