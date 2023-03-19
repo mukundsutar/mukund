e7ee64ef492554e8ff90df30b1953c17761a6145
Mukund Sutar <mukundsutar1512@gmail.com>
2023-03-19 23:12:23 +0530

    Test No.1 - copy button

--- js/copy.js
+++ js/copy.js
@@ -1,14 +1,5 @@
-function copyToClipboard() {
-    // Get the text field
-    var copyText = document.getElementsByClassName("icons");
-  
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-  
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
-  } 
+function copyText(){
+  const text = document.getElementById('text').innerText
+  const btnText = document.getElementById('btn')
+  navigator.clipboard.writeText(text);
+}
