2ca22c1296ca5a766a900aee8365c914eedb8c82
Mukund Sutar <mukundsutar1512@gmail.com>
2023-07-27 23:04:16 +0530

    Fixed a major problem on container overlapping .right and also added animation dor skill section

---
+++ js/dist/accord.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var carerForm = document.getElementById('carerForm');
+console.log('carerForm', carerForm);
+carerForm.addEventListener('submit', function (e) {
+  alert();
+  e.preventDefault();
+  var action = this.attr('action');
+  console.log('action', action);
+  $.ajax({
+    url: "https://fiddle.jshell.net/favicon.png",
+    data: {
+      name: formElements['name'].value,
+      surname: formElementsT['surname'].value,
+      mail: formElementsT['mail'].value,
+      phone: formElements['phone'].value,
+      vacancy: formElements['vacancy'].value,
+      file: formElements['file'].value
+    },
+    success: function success(response) {
+      console.log(response);
+    }
+  });
+});
